2c1919d3e0c2d2807e0c3f314c290a74024e7c14
Abdullah Algamdi <abdm.10@gmail.com>
2022-04-16 07:09:26 +1000

    NEW: Create initial files for migration of upload-submission-modal

---
+++ src/app/tasks/modals/upload-submission-modal/upload-submission-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'upload-submission-modal',
+  templateUrl: './upload-submission-modal.component.html',
+  styleUrls: ['./upload-submission-modal.component.scss']
+})
+export class UploadSubmissionModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
