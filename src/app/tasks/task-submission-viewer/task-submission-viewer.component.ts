5cc9d698c34f171a2c9be4c63d7d4bd084878498
Gradox9 <63984286+Gradox9@users.noreply.github.com>
2022-04-16 10:44:17 +1000

    refactor(taskSubmissionViewer): migration progress

--- src/app/tasks/task-submission-viewer/task-submission-viewer.component.ts
+++ src/app/tasks/task-submission-viewer/task-submission-viewer.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit, Inject, Input, ViewEncapsulation, Output, EventEmitter, Directive } from '@angular/core';
-import { TaskSubmissionService } from 'src/app/common/services/task-submission.service';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
   selector: 'task-submission-viewer',
   templateUrl: './task-submission-viewer.component.html',
   styleUrls: ['task-submission-viewer.component.scss'],
+  
 })
 export class TaskSubmissionViewerComponent {
+  @Input() task: any;
+  @Input() notSubmitted: (task: any) => boolean;
+  @Input() loadingDetails: (task: any) => any;
+  @Input() taskUrl: any;
+  @Input() taskFilesURL: any;
+  @Input() TaskFeedback: { getTaskUrl: (arg0: any) => any; getTaskFilesUrl: (arg0: any) => any; }
   
+  newTask: { getSubmissionDetails: () => void; }
+  
+  constructor() {
+    this.notSubmitted = (task: { has_pdf: any; processing_pdf: any; }) => !task.has_pdf && (!task.processing_pdf);
+
+    this.loadingDetails = (task: { needsSubmissionDetails: () => any; }) => task.needsSubmissionDetails();
+
+      
+    if (this.newTask == null) { return; }
+      this.newTask.getSubmissionDetails();
+      this.taskUrl = this.TaskFeedback.getTaskUrl(this.newTask);
+      return this.taskFilesURL = this.TaskFeedback.getTaskFilesUrl(this.newTask);
+    }
 }
 
 /*
