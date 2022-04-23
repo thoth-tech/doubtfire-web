97b34adb9fa4351434e2e3054c93ba806d50ff3b
A Luan Luong <leo.aluong@gmail.com>
2022-04-23 14:39:46 +1000

    MIGRATE: Create initial files for migration of portfolio-welcome-step

---
+++ src/app/projects/states/portfolio/directives/portfolio-welcome-step/portfolio-welcome-step.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-welcome-step',
+  templateUrl: 'portfolio-welcome-step.component.html',
+
+})
+export class PortfolioWelcomeStepComponent {
+  constructor() {}
+}
