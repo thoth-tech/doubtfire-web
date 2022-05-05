11d32115b5890b1e1766aec9dc5d76e2e1a3b084
Gradox9 <63984286+Gradox9@users.noreply.github.com>
2022-05-05 12:07:11 +1000

    refactor: continued migration of fileuploader

--- src/app/common/file-uploader/file-uploader.component.ts
+++ src/app/common/file-uploader/file-uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'file-uploader',
@@ -8,38 +9,36 @@ import { Component, Input, } from '@angular/core';
 export class FileUploaderComponent {
     @Input() upload: any;
     @Input() files: { length: any; };
-    @Input() clearEnqueuedUpload: { (arg0: any): any; (upload: any): {}; };
-    @Input() showName: boolean;
-    @Input() singleDropZone: boolean;
-    @Input() asButton: boolean;
-    @Input() showUploader: boolean;
-    @Input() showUploadButton: boolean;
-    @Input() resetAfterUpload: boolean;
-    @Input() modelChanged: (newFiles: any, upload: any) => {};
-    @Input() filesSelected: any;
-    @Input() uploadZones: any;
-    @Input() selectedFiles: any;
-    @Input() shownUploadZones: {};
-    @Input() dropSupported: boolean;
-    @Input() $watch: (arg0: string, arg1: (files: any, oldFiles: any) => any) => void;
-    @Input() readyToUpload: { (): any; (): boolean; };
-    @Input() isReady: boolean;
-    @Input() resetUploader: { (): void; (): any; };
-    @Input() uploadingInfo: { complete: any; success: any; error: any; progress: any; };
-    @Input() isUploading: boolean;
-    @Input() onClickFailureCancel: any;
-    @Input() initiateUpload: () => any;
-    @Input() onBeforeUpload: () => void;
-    @Input() payload: { [x: string]: any; };
-    @Input() onSuccess: (arg0: any) => void;
-    @Input() onComplete: () => void;
-    @Input() onFailure: (arg0: any) => void; 
-    @Input() $apply: () => any; 
-    @Input() method: string; 
-    @Input() url: any;
-    @Input() $timeout: (arg0: { (): boolean; (): any; (): any; }, arg1: number) => void; 
-    @Input() currentUser: { authenticationToken: any; profile: { username: any; }};
-
+    clearEnqueuedUpload: { (arg0: any): any; (upload: any): {}; };
+    showName: boolean;
+    singleDropZone: boolean;
+    asButton: boolean;
+    showUploader: boolean;
+    showUploadButton: boolean;
+    resetAfterUpload: boolean;
+    modelChanged: (newFiles: any, upload: any) => void;
+    filesSelected: any;
+    uploadZones: any;
+    selectedFiles: any;
+    shownUploadZones: {};
+    dropSupported: boolean;
+    watch: (arg0: string, arg1: (files: any, oldFiles: any) => any) => void;
+    readyToUpload: { (): any; (): boolean; };
+    isReady: boolean;
+    resetUploader: { (): void; (): any; };
+    uploadingInfo: { complete: any; success: any; error: any; progress: any; };
+    isUploading: boolean;
+    onClickFailureCancel: any;
+    onBeforeUpload: () => void;
+    payload: { [x: string]: any; };
+    onSuccess: (arg0: any) => void;
+    onComplete: () => void;
+    onFailure: (arg0: any) => void; 
+    method: string; 
+    url: any;
+    setTimeout: (arg0: { (): boolean; (): any; (): any; }, arg1: number) => void
+    currentUser: { authenticationToken: any; profile: { username: any; }}; //Grady - possibly need to inject currentuser
+    initiateUpload: () => any;
 
     bindings: {
     // Files map a key (file name to be uploaded) to a value (containing a
@@ -182,5 +181,205 @@ export class FileUploaderComponent {
       return false;
     };
 
+    // Called when the model has changed
+    this.modelChanged = function(newFiles: { length: number; }, upload: { rejects: { length: number; }, display: {error:boolean} }) {
+      if (!(newFiles.length > 0) && !(upload.rejects.length > 0)) { return; }
+      const gotError = checkForError(upload);
+      if (!gotError) {
+        this.filesSelected = _.flatten(_.map(this.uploadZones, 'model'));
+        if (this.singleDropZone) {
+          this.selectedFiles = this.uploadZones;
+          return refreshShownUploadZones();
+        }
+      }
+    };
+
+    //
+    // Will refresh which shown drop zones are shown
+    // Only changes if showing one drop zone
+    //
+    var refreshShownUploadZones = function() {
+      if (this.singleDropZone) {
+        // Find the first-most empty model in each zone
+        const firstEmptyZone = _.find(this.uploadZones, (zone: { model: { length: number; }; }) => (zone.model == null) || (zone.model.length === 0));
+        if (firstEmptyZone != null) {
+          return this.shownUploadZones = [firstEmptyZone];
+        } else {
+          return this.shownUploadZones = [];
+        }
+      }
+    };
+
+    //
+    // Whether or not drop is supported by this browser - assume
+    // true initially, but the drop zone will alter this
+    //
+    this.dropSupported = true;
+
+    const createUploadZones = function(files: any) {
+      const zones = _.map(files, function(uploadData: { name?: any; type?: any; }, uploadName: any) {
+        const {
+          type
+        } = uploadData;
+        const typeData = ACCEPTED_TYPES[type];
+        // No typeData found?
+        if (typeData == null) {
+          throw Error(`Invalid type provided to File Uploader ${type}`);
+        }
+        const zone = {
+          name:     uploadName,
+          model:    null,
+          accept:   "'." + typeData.extensions.join(',.') + "'",
+          // Rejected files
+          rejects:  null,
+          display: {
+            name:   uploadData.name,
+            // Font awesome supports PDF (from Document),
+            // CSV, Code and Image icons
+            icon:   typeData.icon,
+            type:   typeData.name,
+            // Whether or not a reject error is shown
+            error:  false
+          }
+        };
+        return zone;
+      });
+      // Remove all but the active drop zone
+      if (this.singleDropZone) {
+        this.shownUploadZones = [_.first(zones)];
+      } else {
+        this.shownUploadZones = zones;
+      }
+      return this.uploadZones = zones;
+    };
+    createUploadZones(this.files);
+
+    //
+    // Watch for changes in the files, and recreate the zones when
+    // they do change
+    //
+    this.watch('files', (files: any, oldFiles: any) => createUploadZones(files));
+
+    //
+    // Checks if okay to upload (i.e., file models exist for each drop zone)
+    //
+    this.readyToUpload = () => this.isReady = _.compact(_.flatten((Array.from(this.uploadZones).map((upload: { model: any; }) => upload.model)))).length === _.keys(this.files).length;
+
+    //
+    // Resets the uploader and call it
+    //
+    this.resetUploader = function() {
+      // No upload info and we're not uploading
+      this.uploadingInfo = null;
+      this.isUploading = false;
+      this.showUploader = !this.asButton;
+      return Array.from(this.uploadZones).map((upload: any) =>
+        this.clearEnqueuedUpload(upload));
+    };
+    this.resetUploader();
+    
+    //
+    // Override on click failure cancel if not set to just reset uploader
+    //
+    if (this.onClickFailureCancel == null) { this.onClickFailureCancel = this.resetUploader; }
+  
+    //
+    // Initiates the upload
+    //
+    this.initiateUpload = function() {
+      if (!this.readyToUpload()) { return; }
+      if (typeof this.onBeforeUpload === 'function') {
+        this.onBeforeUpload();
+      }
+
+      const xhr   = new XMLHttpRequest();
+      const form  = new FormData();
+      // Append data
+      const files = (Array.from(this.uploadZones).map((zone: { name: any; model: {}; }) => ({ name: zone.name, data: zone.model[0] })));
+      for (let file of Array.from(files)) { form.append(file.name, file.data); }
+      // Append payload
+      const payload = ((() => {
+        const result = [];
+        for (let k in this.payload) {
+          const v = this.payload[k];
+          result.push({ key: k, value: v });
+        }
+        return result;
+      })());
+      for (let payloadItem of Array.from(payload)) {
+        if (_.isObject(payloadItem.value)) { payloadItem.value = JSON.stringify(payloadItem.value); }
+        form.append(payloadItem.key, payloadItem.value);
+      }
+      // Set the percent
+      this.uploadingInfo = {
+        progress: 5,
+        success: null,
+        error: null,
+        complete: false
+      };
+      this.isUploading = true;
+      // Callbacks
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          return setTimeout((function() {
+            // Upload is now complete
+            this.uploadingInfo.complete = true;
+            let response = null;
+            try {
+              response = JSON.parse(xhr.responseText);
+            } catch (e) {
+              if (xhr.status === 0) {
+                response = { error: 'Could not connect to the Doubtfire server' };
+              } else {
+                response = xhr.responseText;
+              }
+            }
+            // Success (20x success range)
+            if ((xhr.status >= 200) && (xhr.status < 300)) {
+              if (typeof this.onSuccess === 'function') {
+                this.onSuccess(response);
+              }
+              this.uploadingInfo.success = true;
+              setTimeout((function() {
+                if (typeof this.onComplete === 'function') {
+                  this.onComplete();
+                }
+                if (this.resetAfterUpload) {
+                  return this.resetUploader();
+                }
+              }), 2500);
+            // Fail
+            } else {
+              if (typeof this.onFailure === 'function') {
+                this.onFailure(response);
+              }
+              this.uploadingInfo.success = false;
+              this.uploadingInfo.error   = response.error || "Unknown error";
+            }
+            return this.apply();
+          }), 2000);
+        }
+      };
+
+      xhr.upload.onprogress = function(event) {
+        this.uploadingInfo.progress = parseInt((100.0 * event.position) / event.totalSize);
+        return this.apply();
+      };
+
+      // Default the method to POST if it was not defined
+      if (this.method == null) { this.method = 'POST'; }
+
+      // Send it
+      xhr.open(this.method, this.url, true);
+
+      // Add auth details
+      xhr.setRequestHeader('Auth-Token', this.currentUser.authenticationToken); //Grady - added this. to current users but still might need to do injection?
+      xhr.setRequestHeader('Username', this.currentUser.profile.username);
+
+      return xhr.send(form);
+    };
+  
   }
+
+
 }
