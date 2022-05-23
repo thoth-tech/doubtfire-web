23fd6cbb5cb5ef864c02619850c820bd93124e63
Gradox9 <63984286+Gradox9@users.noreply.github.com>
2022-05-24 09:26:34 +1000

    refactor: fixed api bug and added error handling

--- src/app/common/file-uploader/file-uploader.component.ts
+++ src/app/common/file-uploader/file-uploader.component.ts
@@ -1,20 +1,17 @@
 //IMPORTANT: READ ALL COMMENTS BEFORE BEINGING MIGRATION
 
-import { Component, Inject, Input, } from '@angular/core';
+import { Component, Inject, Input, OnInit, } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as _ from 'lodash';
 import { currentUser } from 'src/app/ajs-upgraded-providers';
 import API_URL from 'src/app/config/constants/apiURL';
-
-//FIX: remove https://github.com/danialfarid/ng-file-upload from code as HttpClient can perform upload instead
-
 
 @Component({
   selector: 'file-uploader',
   templateUrl: 'file-uploader.component.html',
   styleUrls: ['file-uploader.component.scss'],
 })
-export class FileUploaderComponent {
+export class FileUploaderComponent implements OnInit{
     @Input() upload: any;
     @Input() files: { length: any; };
     clearEnqueuedUpload: { (arg0: any): any; (upload: any): {}; };
@@ -47,95 +44,10 @@ export class FileUploaderComponent {
     setTimeout: (arg0: { (): boolean; (): any; (): any; }, arg1: number) => void
     filename: string;
     httpClient: any;
-
-    //TODO: Remove bindings
-    bindings: {
-    // Files map a key (file name to be uploaded) to a value (containing a
-    // a display name, and the type of file that is to be accepted, where
-    // type is one of [document, csv, archive, code, image]
-    // E.g.:
-    // { file0: { name: 'Silly Name Code', type: 'code'  },
-    //   fileX: { name: 'Silly name Shot', type: 'image' } ... }
-    files: '=',
-    // URL to where image is to be uploaded
-    url: '=',
-    // Optional HTTP method used to post data (defaults to POST)
-    method: '@',
-    // Other payload data to pass in the upload
-    // E.g.:
-    // { unit_id: 10, other: { key: data, with: [array, of, stuff] } ... }
-    payload: '=?',
-    // Optional function to notify just prior to upload, enables injection of payload for example
-    onBeforeUpload: '=?',
-    // Optional function to perform on success (with one response parameter)
-    onSuccess: '=?',
-    // Optional function to perform on failure (with one response parameter)
-    onFailure: '=?',
-    // Optional function to perform when the upload is successful and about
-    // to go back into its default state
-    onComplete: '=?',
-    // This value is bound to whether or not the uploader is currently uploading
-    isUploading: '=?',
-    // This value is bound to whether or not the uploader is ready to upload
-    isReady: '=?',
-    // Shows the names of files to be uploaded (defaults to true)
-    showName: '=?',
-    // Shows initially as button
-    asButton: '=?',
-    // Exposed files that are in the zone
-    filesSelected: '=?',
-    // Whether we have one or many drop zones (default is false)
-    singleDropZone: '=?',
-    // Whether or not we show the upload button or do we hide it allowing an
-    // external trigger to upload (default is true)
-    showUploadButton: '=?',
-    // Sets this scope variable to a function that can then be triggered externally
-    // from outside the scope
-    initiateUpload: '=?',
-    // What happens when we click cancel on failure
-    onClickFailureCancel: '=?',
-    // Whether we should reset after upload
-    resetAfterUpload: '=?'  
-    }
-
+    
 
 
   constructor(public http: HttpClient, @Inject(currentUser) private CurrentUser: any) { 
-    //
-    // Accepted upload types with associated data
-    //
-    // TODO: Move Accepted Types into a OnInit() method, public variable -> outside contructor?
-    //
-    const ACCEPTED_TYPES = {
-      document: {
-        extensions: ['pdf', 'ps'],
-        icon:       'fa-file-pdf-o',
-        name:       'PDF'
-      },
-      csv: {
-        extensions: ['csv','xls','xlsx'],
-        icon:       'fa-file-excel-o',
-        name:       'CSV'
-      },
-      code: {
-        extensions: ['pas', 'cpp', 'c', 'cs', 'h', 'java', 'py', 'js', 'html', 'coffee', 'rb', 'css',
-                    'scss', 'yaml', 'yml', 'xml', 'json', 'ts', 'r', 'rmd', 'rnw', 'rhtml', 'rpres', 'tex',
-                    'vb', 'sql', 'txt', 'md', 'jack', 'hack', 'asm', 'hdl', 'tst', 'out', 'cmp', 'vm', 'sh', 'bat',
-                    'dat'],
-        icon:       'fa-file-code-o',
-        name:       'code'
-      },
-      image: {
-        extensions: ['png', 'bmp', 'tiff', 'tif', 'jpeg', 'jpg', 'gif'],
-        name:       'image',
-        icon:       'fa-file-image-o'
-      },
-      zip: {
-        extensions: ['zip', 'tar.gz', 'tar'],
-        name:       'archive',
-        icon:       'fa-file-zip-o'
-      }
-    };
     
     //
     // Error handling; check if empty files
@@ -144,8 +56,6 @@ export class FileUploaderComponent {
   
     //
     // Whether or not clearEnqueuedFiles is enabled
-    // FIX: upload.model is never past out of the function
-    //
     function clearEnqueuedUpload(upload:{model: any;}){
       upload.model = null;
       return refreshShownUploadZones();
@@ -233,6 +143,64 @@ export class FileUploaderComponent {
     //
     this.dropSupported = true;
 
+    //
+    // Checks if okay to upload (i.e., file models exist for each drop zone)
+    //
+    this.readyToUpload = () => this.isReady = _.compact(_.flatten((Array.from(this.uploadZones).map((upload: { model: any; }) => upload.model)))).length === _.keys(this.files).length;
+
+    //
+    // Resets the uploader and call it
+    //
+    function resetUploader(){
+      // No upload info and we're not uploading
+      this.uploadingInfo = null;
+      this.isUploading = false;
+      this.showUploader = !this.asButton;
+      return Array.from(this.uploadZones).map((upload: any) =>
+        clearEnqueuedUpload(upload));
+    };
+    resetUploader();
+
+    //
+    // Override on click failure cancel if not set to just reset uploader
+    //
+    if (this.onClickFailureCancel == null) { this.onClickFailureCancel = resetUploader; }
+  }
+  
+    ngOnInit(){
+    //
+    // Accepted upload types with associated data
+    //
+    const ACCEPTED_TYPES = {
+      document: {
+        extensions: ['pdf', 'ps'],
+        icon:       'fa-file-pdf-o',
+        name:       'PDF'
+      },
+      csv: {
+        extensions: ['csv','xls','xlsx'],
+        icon:       'fa-file-excel-o',
+        name:       'CSV'
+      },
+      code: {
+        extensions: ['pas', 'cpp', 'c', 'cs', 'h', 'java', 'py', 'js', 'html', 'coffee', 'rb', 'css',
+                    'scss', 'yaml', 'yml', 'xml', 'json', 'ts', 'r', 'rmd', 'rnw', 'rhtml', 'rpres', 'tex',
+                    'vb', 'sql', 'txt', 'md', 'jack', 'hack', 'asm', 'hdl', 'tst', 'out', 'cmp', 'vm', 'sh', 'bat',
+                    'dat'],
+        icon:       'fa-file-code-o',
+        name:       'code'
+      },
+      image: {
+        extensions: ['png', 'bmp', 'tiff', 'tif', 'jpeg', 'jpg', 'gif'],
+        name:       'image',
+        icon:       'fa-file-image-o'
+      },
+      zip: {
+        extensions: ['zip', 'tar.gz', 'tar'],
+        name:       'archive',
+        icon:       'fa-file-zip-o'
+      }
+    };
 
     //
     // Data required for each upload zone
@@ -275,71 +243,52 @@ export class FileUploaderComponent {
       return this.uploadZones = zones;
     };
     createUploadZones(this.files);
-
+    
     //
     // Watch for changes in the files, and recreate the zones when they do change
     //
     this.watch('files', (files: any, oldFiles: any) => createUploadZones(files));
+    }
 
     //
-    // Checks if okay to upload (i.e., file models exist for each drop zone)
+    // Imports API directory and assigns it
     //
-    this.readyToUpload = () => this.isReady = _.compact(_.flatten((Array.from(this.uploadZones).map((upload: { model: any; }) => upload.model)))).length === _.keys(this.files).length;
-
-    //
-    // Resets the uploader and call it
-    //
-    function resetUploader(){
-      // No upload info and we're not uploading
-      this.uploadingInfo = null;
-      this.isUploading = false;
-      this.showUploader = !this.asButton;
-      return Array.from(this.uploadZones).map((upload: any) =>
-        clearEnqueuedUpload(upload));
-    };
-    resetUploader();
-
-    //
-    // Override on click failure cancel if not set to just reset uploader
-    //
-    if (this.onClickFailureCancel == null) { this.onClickFailureCancel = resetUploader; }
+    public API_URL: string = API_URL;
     
     //
-    // Initiates the upload using HttpClient
-    // Grady - This is the new uploading function
+    // Initiates HttpClient Upload
     //
-    // Add success and failure errors
-    //
-  }
-  
-  //TODO: Put into ngInit?
-  public initiateUpload(){
-    if (!this.readyToUpload()) { return; }
-    const file:File = event.target.files[0]
-    if(file){
-      this.filename = file.name;
-      const form = new FormData();
-       // Append data
-    const files = (Array.from(this.uploadZones).map((zone: { name: any; model: {}; }) => ({ name: zone.name, data: zone.model[0] })));
-    for (let file of Array.from(files)) { form.append(file.name, file.data); }
-    // Append payload
-    const payload = ((() => {
-      const result = [];
-      for (let k in this.payload) {
-        const v = this.payload[k];
-        result.push({ key: k, value: v });
+    public initiateUpload(upload:any){
+      if (!this.readyToUpload()) { return; }
+      const file:File = upload.target.files[0]
+      if(file){
+        this.filename = file.name;
+        const form = new FormData();
+        // Append data
+      const files = (Array.from(this.uploadZones).map((zone: { name: any; model: {}; }) => ({ name: zone.name, data: zone.model[0] })));
+      for (let file of Array.from(files)) { form.append(file.name, file.data); }
+      // Append payload
+      const payload = ((() => {
+        const result = [];
+        for (let k in this.payload) {
+          const v = this.payload[k];
+          result.push({ key: k, value: v });
+        }
+        return result;
+      })());
+      for (let payloadItem of Array.from(payload)) {
+        if (_.isObject(payloadItem.value)) { payloadItem.value = JSON.stringify(payloadItem.value); }
+        form.append(payloadItem.key, payloadItem.value);
       }
-      return result;
-    })());
-    for (let payloadItem of Array.from(payload)) {
-      if (_.isObject(payloadItem.value)) { payloadItem.value = JSON.stringify(payloadItem.value); }
-      form.append(payloadItem.key, payloadItem.value);
-    } 
-    const upload = this.httpClient.post(this.API_URL, form, {headers: new HttpHeaders().set('Auth-Token', this.CurrentUser.authenticationToken)}, 
-    {headers: new HttpHeaders().set('Username', this.CurrentUser.profile.username)});
-    upload.subscribe({
-      //TODO: Insert Error Handling - https://rxjs.dev/deprecations/subscribe-arguments
-    })
+      // Uploads File/s to API
+      const upload = this.httpClient.post(this.API_URL, form, {headers: new HttpHeaders().set('Auth-Token', this.CurrentUser.authenticationToken)}, 
+      {headers: new HttpHeaders().set('Username', this.CurrentUser.profile.username)});
+      upload.subscribe({
+        // Error handling - https://rxjs.dev/deprecations/subscribe-arguments
+        next: (v:any) => console.log(v),
+        error: (e:any) => console.error("An error occured",e),
+        complete: () => console.info('complete')
+        })
+      }
     }
-  }
 }
