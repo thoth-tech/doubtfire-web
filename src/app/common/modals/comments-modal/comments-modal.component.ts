a4da9ff786f61231d647b0d9dc7e499dccf65089
Minh Tho DUONG <s223749059@deakin.edu.au>
2024-03-26 14:22:01 +1100

    refactor: migrate the comments-modal - (initial)

---
+++ src/app/common/modals/comments-modal/comments-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'comments-modal',
+  templateUrl: './comments-modal.component.html',
+  styleUrls: ['./comments-modal.component.scss'],
+})
+export class CommentsModalComponent {
+  constructor() {}
+}
--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -95,6 +95,7 @@ import { ExtensionCommentComponent } from './tasks/task-comments-viewer/extensio
 import { CampusListComponent } from './admin/institution-settings/campuses/campus-list/campus-list.component';
 import { ExtensionModalComponent } from './common/modals/extension-modal/extension-modal.component';
 import { CalendarModalComponent } from './common/modals/calendar-modal/calendar-modal.component';
+import { CommentsModalComponent } from './common/modals/comments-modal/comments-modal.component';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MAT_DATE_LOCALE, MatOptionModule } from '@angular/material/core';
@@ -250,6 +251,7 @@ import { TasksViewerComponent } from './units/states/tasks/tasks-viewer/tasks-vi
     OverseerImageListComponent,
     ExtensionModalComponent,
     CalendarModalComponent,
+    CommentsModalComponent,
     InstitutionSettingsComponent,
     HomeComponent,
     CommentBubbleActionComponent,
