5830f712b6d8bba9bb4dd32e1c42816f9cd6f33a
Riley Helmore <rileyhelmore@gmail.com>
2024-04-08 10:22:41 +1000

    Create initial files for migration of grade-modal-task

---
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'GradeTaskModal',
+  templateUrl: 'tasks/modals/grade-task-modal/grade-task-modal.component.html',
+  styleUrls: ['grade-task-modals.component.scss'],
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+}
