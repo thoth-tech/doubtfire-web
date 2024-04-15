0971f525803c94b143ac65843852af4c2cd9832f
Riley Shannon <rashannon@deakin.edu.au>
2024-04-15 10:06:58 +1000

    NEW Create initial files for migration of group member contribution assigner

---
+++ src/app/groups/group-member-contribution-assigner/group-member-contribution-assigner.component.ts
@@ -0,0 +1,10 @@
+import{Component, Input, Inject} from '@angular/core';
+
+@Component({
+  selector: 'group-member-contribution-assigner',
+  templateUrl: 'group-member-contribution-assigner.component.html',
+  styleUrls: ['group-member-contribution-assigner.component.scss'],
+})
+export class GroupMemberContributionAssignerComponent {
+  constructor(){}
+}
