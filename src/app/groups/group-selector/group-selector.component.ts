4875b518e02acc18102901f479b374769fe04544
Darren Sunandar <darrenmar14@gmail.com>
2024-04-18 14:51:54 +1000

    commit

---
+++ src/app/groups/group-selector/group-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'group-selector',
+  templateUrl: 'group-selector.component.html',
+  styleUrls: ['group-selector.component.scss'],
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+}
