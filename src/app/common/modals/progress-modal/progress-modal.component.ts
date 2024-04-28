3b0dbf7d6197f295c06180d10168ae290dab2287
rileyhelmore <rileyhelmore@gmail.com>
2024-04-28 22:02:00 +1000

    migration for progress modal

---
+++ src/app/common/modals/progress-modal/progress-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'progress-modal',
+  templateUrl: './progress-modal.component.html',
+})
+export class ProgressModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  //method to close modal
+  close(): void {
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/common/modals/progress-modal/progress-modal.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProgressModalComponent } from './progress-modal.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProgressModalService {
+  constructor(private dialog: MatDialog) {}
+
+  //method to show progress modal
+  show(title: string, message: string, promise?: Promise<any>): void {
+    const dialogRef = this.dialog.open(ProgressModalComponent, {
+      data: {
+        title: title,
+        message: message
+      }
+    });
+
+    if (promise) {
+      promise.finally(() => {
+        dialogRef.close();
+      });
+    }
+  }
+}
