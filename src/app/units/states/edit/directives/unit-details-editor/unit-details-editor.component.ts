9d5f6a79a36ac27e114e008062787dd74cabf2cf
sammyrolfe <sam-rolfe@hotmail.com>
2024-05-02 06:24:51 +0000

    Created initial files for migration

---
+++ src/app/units/states/edit/directives/unit-details-editor/unit-details-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'unit-details-editor',
+  templateUrl: 'task-description-card.component.html',
+  styleUrls: ['task-description-card.component.scss'],
+})
+
+export class UnitDetailsEditor {
+  constructor() {}
+}
