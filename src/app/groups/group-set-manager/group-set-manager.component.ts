442c316a16368882def98e46bfd16981a6039dad
s223126445 <s223126445@deakin.edu.au>
2024-05-06 04:17:59 +1000

    new:migration of group set manager

---
+++ src/app/groups/group-set-manager/group-set-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'f-app-group-set-manager',
+  templateUrl: 'group-set-manager.component.html',
+  styleUrls: ['/group-set-manager.component.scss'],
+})
+export class GroupSetManagerComponent {
+  constructor() {}
+}
