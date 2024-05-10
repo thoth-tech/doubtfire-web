a1057e5e7d2a1819cb00728b97a662c0b8a5c64e
PrabhKamboj <officialprabhkamboj@gmail.com>
2024-05-10 16:42:07 +1000

    feat: create initial files for migration of upload-submission-modal

---
+++ src/app/tasks/modals/upload-submission-modal/upload-submission-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'f-upload-submission-modal',
+  templateUrl: 'upload-submission-modal.component.html',
+  styleUrls: ['upload-submission-modal.component.scss'],
+})
+export class UploadSubmissionModalComponent {
+  constructor() {}
+}
