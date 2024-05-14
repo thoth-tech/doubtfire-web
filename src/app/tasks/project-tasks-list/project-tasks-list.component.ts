3d193754dffdc736b31d859d3400ead2ff825f42
PrabhKamboj <officialprabhkamboj@gmail.com>
2024-05-14 19:46:45 +1000

    feat: create initial files for migration of project-tasks-list

---
+++ src/app/tasks/project-tasks-list/project-tasks-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'project-tasks-list',
+  templateUrl: 'project-tasks-list.component.html',
+  styleUrls: ['project-tasks-list.component.scss'],
+})
+export class ProjectTasksListComponent {
+  constructor() {}
+}
