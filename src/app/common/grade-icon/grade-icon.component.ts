e7cbe793adf340a9b7bcf0522886cabcfec481cf
PrabhKamboj <officialprabhkamboj@gmail.com>
2024-05-16 23:39:36 +1000

    feat: create initial files for migration of grade-icon

---
+++ src/app/common/grade-icon/grade-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'grade-icon',
+  templateUrl: 'grade-icon.component.html',
+  styleUrls: ['grade-icon.component.scss'],
+})
+export class GradeIconComponent {
+  constructor() {}
+}
