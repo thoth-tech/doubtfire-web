28bf99fd93e39a738a74acf465d57b04f6356618
PrabhKamboj <officialprabhkamboj@gmail.com>
2024-05-16 23:39:27 +1000

    feat: create initial files for migration of grade-task-modal

---
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'grade-task-modal',
+  templateUrl: 'grade-task-modal.component.html',
+  styleUrls: ['grade-task-modal.component.scss'],
+})
+export class GradeTaskModalComponent {
+  constructor() {}
+}
