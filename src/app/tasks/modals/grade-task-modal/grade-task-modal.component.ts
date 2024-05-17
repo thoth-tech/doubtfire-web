895356fd8ce5f65535512e6f8503e99289ab26fb
PrabhKamboj <officialprabhkamboj@gmail.com>
2024-05-17 19:17:54 +1000

    refactor: migrate grade-task-modal component to TypeScript

--- src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -1,10 +1,81 @@
-import { Component, Input, Inject } from '@angular/core';
+import {AfterViewInit, Component, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {GradeService} from 'src/app/common/services/grade.service';
+import {Task} from 'src/app/api/models/task';
+
+//
+// Open a grade task modal with the provided task
+//
 
 @Component({
-  selector: 'grade-task-modal',
-  templateUrl: 'grade-task-modal.component.html',
-  styleUrls: ['grade-task-modal.component.scss'],
+  selector: 'f-grade-task-modal',
+  templateUrl: './grade-task-modal.component.html',
+  styleUrl: './grade-task-modal.component.scss',
 })
 export class GradeTaskModalComponent {
-  constructor() {}
+
+  data: {
+    desiredGrade: number;
+    rating: number;
+    overStar: number;
+    confRating: number;
+  };
+
+  numStars: number;
+  gradeValues: number[];
+  grades: string[];
+  task: Task;
+
+  constructor(
+    public dialogRef: MatDialogRef<GradeTaskModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public dialogData: {task: Task},
+    private gradeService: GradeService,
+  ) {
+
+    this.task = dialogData.task;
+
+    this.data = {
+      desiredGrade: dialogData.task.grade,
+      rating:
+        !dialogData?.task?.qualityPts || dialogData.task.qualityPts < 0
+          ? 2
+          : dialogData.task.qualityPts,
+      overStar: 0,
+      confRating: 0,
+    };
+
+    this.numStars = dialogData.task.definition.maxQualityPts || 5;
+    this.gradeValues = this.gradeService.allGradeValues;
+    this.grades = this.gradeService.grades;
+  }
+
+
+  // Method to handle the dialog close with data
+  close(): void {
+    this.dialogRef.close({
+      qualityPts: this.data.rating,
+      selectedGrade: this.data.desiredGrade,
+    });
+  }
+
+  // Method to handle the dialog close without returning any data
+  dismiss(): void {
+    this.dialogRef.close();
+  }
+
+  // Method to update hover state
+  hoveringOver(value: number): void {
+    this.data.overStar = value;
+  }
+
+  // Logic to handle clearing or setting the rating based on hover interactions
+  checkClearRating(event: any): void {
+    this.data.rating = event;
+    if (this.data.confRating === 1 && this.data.rating === 1 && this.data.overStar === 1) {
+      this.data.rating = 0;
+    } else if (this.data.confRating === 1 && this.data.overStar === 1 && this.data.rating === 0) {
+      this.data.rating = 1;
+    }
+    this.data.confRating = this.data.rating;
+  }
 }
