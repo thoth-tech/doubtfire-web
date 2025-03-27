8a3d39784b4560b883da94b1f693478498ab643a
lachlan-robinson <lachy4242@hotmail.com>
2025-03-27 12:17:08 +1100

    fix: implement analytics service

--- src/app/visualisations/visualisation.service.ts
+++ src/app/visualisations/visualisation.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
-//import {analyticsService} from '../ajs-upgraded-providers';
+import {Inject, Injectable} from '@angular/core';
+import {analyticsService} from '../ajs-upgraded-providers';
 
 @Injectable({
   providedIn: 'root',
@@ -41,7 +41,7 @@ export class VisualisationService {
     debounce: 10,
   };
 
-  constructor() {}
+  constructor(@Inject(analyticsService) private analyticsService) {}
 
   /**
    * Creates a visualisation configuration.
@@ -67,9 +67,7 @@ export class VisualisationService {
     const dirtyOpts = {...this.DEFAULT_OPTS, ...opts, type};
     const dirtyConf = {...this.DEFAULT_CONF, ...conf};
 
-    /*
     this.analyticsService.event('Visualisations', 'Created Visualisation', visualisationName);
-    */
 
     return [{chart: dirtyOpts, title: titleOpts, subtitle: subtitleOpts}, dirtyConf];
   }
