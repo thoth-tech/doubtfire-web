7fe10feb053efa5654b25a6ca22090bd9574daf8
Anmol Saini <sainianmol1922@gmail.com>
2025-05-14 20:26:56 +1000

    feat: make new file for privacy-policy.service.ts and modify angular modules

---
+++ src/app/config/privacy-policy/privacy-policy.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import API_URL from 'src/app/config/constants/apiURL';
+
+interface PrivacyResponse {
+  privacy: string;
+  plagiarism: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrivacyPolicy {
+  privacy = '';
+  plagiarism = '';
+  loaded = false;
+
+  constructor(private http: HttpClient) {
+    const url = `${API_URL}/settings/privacy`;
+
+    this.http.get<PrivacyResponse>(url).subscribe(response => {
+      console.log('Fetched Privacy Policy:', response.privacy);
+      console.log('Fetched Plagiarism Policy:', response.plagiarism);
+      this.privacy = response.privacy;
+      this.plagiarism = response.plagiarism;
+      this.loaded = true;
+      console.log('Data Loaded:', this.loaded);
+    });
+  }
+}
