8a76d310469753bb296e69071652d236d36d8f42
disururathnayake <disuru.office@gmail.com>
2025-05-17 23:55:36 +1000

    chore: create initial files for migration of task-ilo-alignment-modal

---
+++ src/app/tasks/task-ilo-alignment/modals/task-ilo-alignment-modal/task-ilo-alignment-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'task-ilo-alignment-modal',
+  templateUrl: 'task-ilo-alignment-modal.component.html',
+})
+export class TaskIloAlignmentModalComponent {
+  constructor() {}
+}
