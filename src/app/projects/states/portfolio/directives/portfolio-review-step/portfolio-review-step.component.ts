a67ceafeec8e1d0da9f99aed11fb4014bf601be5
Chelaka Yasodhana <70631404+chelaz1234@users.noreply.github.com>
2025-05-24 23:38:59 +1000

    Update portfolio-review-step.component.ts

--- src/app/projects/states/portfolio/directives/portfolio-review-step/portfolio-review-step.component.ts
+++ src/app/projects/states/portfolio/directives/portfolio-review-step/portfolio-review-step.component.ts
@@ -1,34 +1,52 @@
-// This is a fixed and cleaned-up version of portfolio-review-step.component.ts
-
-import {Component, Input, OnInit} from '@angular/core';
-import {AlertService} from 'src/app/common/services/alert.service';
-import {DoubtfireConstants} from 'src/app/config/constants/doubtfire-constants';
-import {FileDownloaderService} from 'src/app/common/file-downloader/file-downloader.service';
-import {ProjectService} from 'src/app/api/services/project.service';
-import {confirmationModal} from 'src/app/ajs-upgraded-providers';
+import { Component, Input, OnInit, Inject } from '@angular/core';
+import { AlertService } from 'src/app/common/services/alert.service';
+import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
+import { FileDownloaderService } from 'src/app/common/file-downloader/file-downloader.service';
+import { ProjectService } from 'src/app/api/services/project.service';
+import { confirmationModal } from 'src/app/ajs-upgraded-providers';
+import {Injector} from '@angular/core';
 
 @Component({
-  selector: 'f-portfolio-review-step',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'portfolio-review-step',
   templateUrl: './portfolio-review-step.component.html',
-  styleUrls: ['./portfolio-review-step.component.scss'],
+  styleUrls: ['./portfolio-review-step.component.css'],
 })
 export class PortfolioReviewStepComponent implements OnInit {
-  @Input() project: any;
-  @Input() unit: any;
+  @Input() project!: {
+    portfolioFiles: any;
+    compilePortfolio?: boolean;
+    portfolioAvailable?: boolean;
+    portfolioStatus?: number;
+    learningSummaryReport?: unknown;
+    portfolioUrl: (download: boolean) => string;
+    deletePortfolio: () => { subscribe: (cb: () => void) => void };
+    extraFiles: unknown[];
+    selectedTasks: unknown[];
+    student?: { username: string };
 
-  hasLSR = false;
-  hasTasksSelected = false;
-  portfolioIsCompiling = false;
-  canCompilePortfolio = false;
-  dialog: any;
-  $scope: any;
+  };
+
+  @Input() unit!: unknown;
+  private $scope: any;
+
+  hasLSR: boolean = false;
+  hasTasksSelected: boolean = false;
+  portfolioIsCompiling: boolean = false;
+  canCompilePortfolio: boolean = false;
 
   constructor(
     public doubtfireConstants: DoubtfireConstants,
     private alertService: AlertService,
     private projectService: ProjectService,
     private fileDownloaderService: FileDownloaderService,
-  ) {}
+    private injector: Injector,
+    @Inject(confirmationModal) private confirmationModalService: {
+      show: (title: string, message: string, action: () => void) => void;
+    },
+  ) {
+    this.$scope = this.injector.get('$scope');
+  }
 
   ngOnInit(): void {
     this.evaluatePortfolioState();
@@ -36,13 +54,13 @@ export class PortfolioReviewStepComponent implements OnInit {
 
   evaluatePortfolioState(): void {
     this.hasLSR = this.projectHasLearningSummaryReport();
-    this.hasTasksSelected = (this.selectedTasks() || []).length > 0;
-    this.portfolioIsCompiling = this.project?.compilePortfolio;
+    this.hasTasksSelected = this.selectedTasks().length > 0;
+    this.portfolioIsCompiling = !!this.project.compilePortfolio;
     this.canCompilePortfolio =
       !this.portfolioIsCompiling &&
       this.hasTasksSelected &&
       this.hasLSR &&
-      !this.project?.portfolioAvailable;
+      !this.project.portfolioAvailable;
   }
 
   toggleCompileProject(): void {
@@ -56,49 +74,43 @@ export class PortfolioReviewStepComponent implements OnInit {
   }
 
   deletePortfolio(): void {
-    const dialogRef = this.dialog.open(confirmationModal, {
-      data: {
-        title: 'Delete Portfolio?',
-        message:
-          'Are you sure you want to delete your portfolio? You will need to recreate your portfolio again if you do so.',
-      },
-    });
-
-    dialogRef.afterClosed().subscribe((confirmed) => {
-      if (confirmed) {
+    this.confirmationModalService.show(
+      'Delete Portfolio?',
+      'Are you sure you want to delete your portfolio? You will need to recreate your portfolio again if you do so.',
+      () => {
         this.project.deletePortfolio().subscribe(() => {
           this.project.portfolioAvailable = false;
           this.project.portfolioStatus = 0;
           this.alertService.message('Portfolio has been deleted!', 5000);
         });
-      } else {
-        this.alertService.message('Delete Portfolio action cancelled', 3000);
       }
-    });
-  }
-
-  downloadPortfolio(): void {
-    this.fileDownloaderService.downloadFile(
-      this.project?.portfolioUrl(true),
-      `${this.project?.student?.username}-portfolio.pdf`,
     );
   }
 
-  extraFiles(): any[] {
-    return this.project?.extraFiles || [];
+  downloadPortfolio(): void {
+    const username = this.project.student?.username || 'portfolio';
+    this.fileDownloaderService.downloadFile(
+      this.project.portfolioUrl(true),
+      `${username}-portfolio.pdf`
+    );
   }
 
-  selectedTasks(): any[] {
-    return this.project?.selectedTasks || [];
+  extraFiles(): unknown[] {
+    return this.project.extraFiles || [];
+  }
+
+  selectedTasks(): unknown[] {
+    return this.project.selectedTasks || [];
   }
 
   projectHasLearningSummaryReport(): boolean {
-    return !!this.project?.learningSummaryReport;
+    return this.project?.portfolioFiles?.filter((file: {idx: number}) => file.idx === 0).length > 0;
   }
 
   goToPreviousStep(): void {
     if (typeof this.$scope?.advanceActiveTab === 'function') {
       this.$scope.advanceActiveTab(-1);
     }
+
   }
 }
