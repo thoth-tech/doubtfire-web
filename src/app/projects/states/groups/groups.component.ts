9c09a852a308c3e9061c1d2ff01429615589f790
Anmol Saini <anmolsaini1922@gmail.com>
2025-05-27 00:34:25 +1000

    chore: modifying new files

--- src/app/projects/states/groups/groups.component.ts
+++ src/app/projects/states/groups/groups.component.ts
@@ -1,17 +1,52 @@
-import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StateService } from '@uirouter/angular';
+import { ProjectService } from 'src/app/api/services/project.service';
 import { Project } from 'src/app/api/models/project';
 import { Unit } from 'src/app/api/models/unit';
-
+import { GroupSet } from 'src/app/api/models/doubtfire-model';
 
 @Component({
-  selector: 'f-projects-groups-state',
-  templateUrl: './groups.tpl.html'
+  selector: 'fProjectsStatesGroups',
+  templateUrl: './groups.component.html',
 })
-export class ProjectsGroupsStateComponent {
-  @Input() project!: Project;
-  @Input() unit!: Unit;
+export class ProjectsStatesGroupsComponent implements OnInit {
+  @Input() projectId: number;
 
-  hasGroupwork(): boolean {
+  project: Project;
+  unit: Unit;
+  selectedGroupSet: GroupSet;
+  pageTitle: string = '';
+  task: string = '';
+  roleWhiteList: string[] = [];
+  loading: boolean = true;
+
+  constructor(
+    private stateService: StateService,
+    private projectService: ProjectService,
+  ) {}
+
+  ngOnInit(): void {
+    this.projectService.fetch(this.projectId).subscribe({
+      next: (project) => {
+        this.project = project;
+        this.unit = project.unit;
+
+        // Set metadata from current route
+        const currentState = this.stateService.current;
+        this.pageTitle = currentState.data.pageTitle;
+        this.task = currentState.data.task;
+        this.roleWhiteList = currentState.data.roleWhiteList;
+
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching project:', error);
+        this.loading = false;
+      },
+    });
+  }
+
+  hasGroupWork(): boolean {
     return this.unit?.hasGroupwork?.() ?? false;
   }
 }
