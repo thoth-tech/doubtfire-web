c966a732bf8a774fadff0cfbf445c7719f2932f1
Anmol Saini <anmolsaini1922@gmail.com>
2025-05-30 22:51:25 +1000

    fix: modify angularjs.module file

--- src/app/doubtfire-angularjs.module.ts
+++ src/app/doubtfire-angularjs.module.ts
@@ -61,7 +61,6 @@ import 'build/src/app/config/analytics/analytics.js';
 import 'build/src/app/config/debug/debug.js';
 import 'build/src/app/projects/projects.js';
 import 'build/src/app/projects/project-progress-dashboard/project-progress-dashboard.js';
-import 'build/src/app/projects/states/groups/groups.js';
 import 'build/src/app/projects/states/feedback/feedback.js';
 import 'build/src/app/projects/states/states.js';
 import 'build/src/app/projects/states/dashboard/directives/progress-dashboard/progress-dashboard.js';
