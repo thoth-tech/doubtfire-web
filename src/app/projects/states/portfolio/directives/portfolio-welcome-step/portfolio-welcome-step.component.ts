1b45511028b4de228db689f394eab207ce9ccdcd
j-talev <j.talev@outlook.com>
2025-07-24 21:24:24 +1000

    refactor(portfolio-welcome-step): create component .ts and .html files

---
+++ src/app/projects/states/portfolio/directives/portfolio-welcome-step/portfolio-welcome-step.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'portfolio-welcome-step',
+  templateUrl: './portfolio-welcome-step.component.html',
+})
+export class PortfolioWelcomeStepComponent {
+
+}
