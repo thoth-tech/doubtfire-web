dc0894121ceb8e8e948e5a719a341e05902d45d9
j-talev <j.talev@outlook.com>
2025-07-26 17:34:30 +1000

    style portfolio-welcome-step

--- src/app/projects/states/portfolio/directives/portfolio-welcome-step/portfolio-welcome-step.component.ts
+++ src/app/projects/states/portfolio/directives/portfolio-welcome-step/portfolio-welcome-step.component.ts
@@ -4,6 +4,7 @@ import {Component, Input} from "@angular/core";
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'portfolio-welcome-step',
   templateUrl: './portfolio-welcome-step.component.html',
+  styleUrl: '../../portfolio.component.scss'
 })
 export class PortfolioWelcomeStepComponent {
   @Input() advanceActiveTab?: (step: number) => void;
