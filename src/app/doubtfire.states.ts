081e017bfb78192dece10d95fa53312ccbc34c67
jvellucci <jvellucci@deakin.edu.au>
2025-08-26 23:01:10 +1000

    feat: Staff Grant Extension routing (WIP)
    - WIP commit for adding Angular UIRouter routing for the `Staff Grant Extension` feature

--- src/app/doubtfire.states.ts
+++ src/app/doubtfire.states.ts
@@ -14,6 +14,7 @@ import {UnitRootState} from './units/unit-root-state.component';
 import {ProjectRootState} from './projects/states/project-root-state.component';
 import { TaskViewerState } from './units/task-viewer/task-viewer-state.component';
 import {ScormPlayerComponent} from './common/scorm-player/scorm-player.component';
+import { UnitAnalyticsComponent } from './units/states/analytics/unit-analytics-route.component'; // Todo @SGE team: Replace with SGE component
 import { Ng2ViewDeclaration } from '@uirouter/angular';
 
 /*
@@ -408,6 +409,20 @@ const ScormPlayerReviewState: NgHybridStateDeclaration = {
   },
   data: {
     pageTitle: 'Preview Scorm Test',
+    roleWhitelist: ['Tutor', 'Convenor', 'Admin'],
+  },
+};
+
+const StaffGrantExtensionState: NgHybridStateDeclaration = { // Todo @Jason: Navigating to this route makes the `TaskDropdown` disappear - fix
+  name: 'units/staff_grant_extension',
+  url: '/units/:unitId/staff_grant_extension',
+  views: {
+    main: {
+      component: UnitAnalyticsComponent, // Todo @SGE team: Replace with SGE component
+    },
+  },
+  data: {
+    pageTitle: 'Staff Grant Extension',
     roleWhitelist: ['Tutor', 'Convenor', 'Admin'],
   },
 };
