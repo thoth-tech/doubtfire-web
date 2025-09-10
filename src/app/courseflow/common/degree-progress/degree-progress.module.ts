c21124df8ab0f511e19469baec10cd28ad3ddc3f
Triet-coder <minhtriet29092001@gmail.com>
2025-09-10 23:10:37 +1000

    feat(courseflow): add progress tracking

---
+++ src/app/courseflow/common/degree-progress/degree-progress.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DegreeProgressComponent } from './degree-progress.component';
+
+@NgModule({
+  declarations: [DegreeProgressComponent],
+  imports: [CommonModule],
+  exports: [DegreeProgressComponent]
+})
+export class DegreeProgressModule { }
