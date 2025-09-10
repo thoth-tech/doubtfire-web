8b275424212c5f8d020cf0fb79c9f6337ed30d6d
lachlan-robinson <lachy4242@hotmail.com>
2025-09-10 11:29:07 +1000

    chore: create new component files

---
+++ src/app/visualisations/achievement-custom-bar-chart/achievement-custom-bar-chart/achievement-custom-bar-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'f-achievement-custom-bar-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './achievement-custom-bar-chart.component.html',
+  styleUrl: './achievement-custom-bar-chart.component.css'
+})
+export class AchievementCustomBarChartComponent {
+
+}
