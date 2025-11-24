28fc20e8ccaff671cd5f546b3c56600eabad8517
mannat2634 <sharmamannat2634@gmail.com>
2025-11-25 06:09:48 +1100

    refactor: create initial files for migration of task-ilo-alignment-modal

---
+++ src/app/tasks/task-ilo-alignment/modals/task-ilo-alignment-modal/task-ilo-alignment-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'task-ilo-alignment-modal',
+  templateUrl: 'task-ilo-alignment-modal.component.html',
+  styleUrls: [],
+})
+export class TaskIloAlignmentModalComponent {
+  constructor() {}
+}
